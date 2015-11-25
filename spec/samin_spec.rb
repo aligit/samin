c672a905a1d305c9041e4055cc1debf54a7d8018
alifakheri <fakheri.ali@gmail.com>
2015-11-25 08:36:12 +0100

    Adds & structure all examples that are expected to be implemented

--- spec/samin_spec.rb
+++ spec/samin_spec.rb
@@ -1,11 +1,69 @@
 require 'spec_helper'
 
 describe Samin do
+  subject { Samin.new }
+
   it 'has a version number' do
     expect(Samin::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'Instantiation' do
+    describe '#initialize' do
+      it 'is a pending example'
+    end
   end
+
+  context 'Configuration' do
+    describe '.conversion_rates' do
+      it 'is a pending example'
+    end
+  end
+
+
+  describe '#currency' do
+    it 'is a pending example'
+  end
+
+  describe '#amount' do
+    it 'is a pending example'
+  end
+
+  describe '#convert_to' do
+    it 'is a pending example'
+  end
+
+  context 'Arithmetics' do
+
+    describe '#+' do
+      it 'is a pending example'
+    end
+
+    describe '#-' do
+      it 'is a pending example'
+    end
+
+    describe '#/' do
+      it 'is a pending example'
+    end
+
+    describe '#*' do
+      it 'is a pending example'
+    end
+  end
+
+  context 'Comparisons' do
+
+    describe '#==' do
+      it 'is a pending example'
+    end
+
+    describe '#<' do
+      it 'is a pending example'
+    end
+
+    describe '#>' do
+      it 'is a pending example'
+    end
+  end
+
 end
