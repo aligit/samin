95ec3bcc9b162ee681ba3719d0cc5a116b806008
alifakheri <fakheri.ali@gmail.com>
2015-11-25 10:35:46 +0100

    Resolves class naming issues. no spec failure anymore

--- lib/samin/money.rb
+++ lib/samin/money.rb
@@ -1,13 +1,11 @@
-module Samin::Money
+class Samin::Money
   # attr_accessor :@@currency_reference_name
   # attr_accessor :@@currency_reference_conversion_rates
 
-  class Money
-    def initialize(args)
-    end
+  def initialize(args)
+  end
 
-    def self.conversion_rates
-      true
-    end
+  def self.conversion_rates
+    true
   end
 end
--- spec/lib/samin/money_spec.rb
+++ spec/lib/samin/money_spec.rb
@@ -1,75 +1,78 @@
 require 'spec_helper'
 
-describe Samin::Money do
-  let(:conf_base_name) { 'EUR' }
-  let(:conf_conversion_rates) {{
-    'USD'     => 1.11,
-    'Bitcoin' => 0.0047
-  }}
-  let(:fifty_eur) { Samin::Money.new(50, 'EUR')  }
+module Samin
+  describe Money do
+    let(:conf_base_name) { 'EUR' }
+    let(:conf_conversion_rates) {{
+      'USD'     => 1.11,
+      'Bitcoin' => 0.0047
+    }}
+    let(:fifty_eur) { Money.new(50, 'EUR')  }
 
-  context 'Instantiation' do
-    describe '#initialize' do
-      it 'is a pending example'
+    context 'Instantiation' do
+      describe '#initialize' do
+        it 'is a pending example'
+      end
     end
-  end
 
-  context 'Configuration' do
-    describe '.conversion_rates' do
-      context 'when conversion rates are properly set' do
-        it 'given currency name and conversion rates, returns true' do
-          expect(Samin::Money.conversion_rates).to be_truthy
+    context 'Configuration' do
+      describe '.conversion_rates' do
+        context 'when conversion rates are properly set' do
+          it 'given currency name and conversion rates, returns true' do
+            expect(Money.conversion_rates).to be_truthy
+          end
+        end
+        context 'when issues are raised while setting conversion rates' do
         end
       end
-      context 'when issues are raised while setting conversion rates' do
+    end
+
+
+    describe '#currency' do
+      it 'is a pending example'
+    end
+
+    describe '#amount' do
+      it 'is a pending example'
+    end
+
+    describe '#convert_to' do
+      it 'is a pending example'
+    end
+
+    context 'Arithmetics' do
+
+      describe '#+' do
+        it 'is a pending example'
+      end
+
+      describe '#-' do
+        it 'is a pending example'
+      end
+
+      describe '#/' do
+        it 'is a pending example'
+      end
+
+      describe '#*' do
+        it 'is a pending example'
       end
     end
-  end
 
+    context 'Comparisons' do
 
-  describe '#currency' do
-    it 'is a pending example'
-  end
+      describe '#==' do
+        it 'is a pending example'
+      end
 
-  describe '#amount' do
-    it 'is a pending example'
-  end
+      describe '#<' do
+        it 'is a pending example'
+      end
 
-  describe '#convert_to' do
-    it 'is a pending example'
-  end
-
-  context 'Arithmetics' do
-
-    describe '#+' do
-      it 'is a pending example'
+      describe '#>' do
+        it 'is a pending example'
+      end
     end
-
-    describe '#-' do
-      it 'is a pending example'
-    end
-
-    describe '#/' do
-      it 'is a pending example'
-    end
-
-    describe '#*' do
-      it 'is a pending example'
-    end
-  end
-
-  context 'Comparisons' do
-
-    describe '#==' do
-      it 'is a pending example'
-    end
-
-    describe '#<' do
-      it 'is a pending example'
-    end
-
-    describe '#>' do
-      it 'is a pending example'
-    end
+    
   end
 end
