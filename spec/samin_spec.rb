3165fec3874997dc21f57b3e497dbeb723cc7900
alifakheri <fakheri.ali@gmail.com>
2015-11-25 09:18:39 +0100

    Set up initial variables that will be used accross different examples

--- spec/samin_spec.rb
+++ spec/samin_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Samin do
   subject { Samin.new }
+  let(:conf_base_name) { 'EUR' }
+  let(:conf_conversion_rates) {{
+    'USD'     => 1.11,
+    'Bitcoin' => 0.0047
+  }}
+  let(:fifty_eur) { Money.new(50, 'EUR')  }
 
   it 'has a version number' do
     expect(Samin::VERSION).not_to be nil
